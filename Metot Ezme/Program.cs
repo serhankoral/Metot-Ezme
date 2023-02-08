2f12426056746cbf839d82c1711a94d959e54b5c
Serhan KORAL <serhan_koral@hotmail.com>
2023-02-08 15:28:34 +0300

    Proje dosyası ekle.

---
+++ Metot Ezme/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metot_Ezme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Metod ezmede kalıtımda base class da olan fonksiyonla yanı isimde yeni fonksiyon kullanılır
+             * bu fonksiyon kullanılırken virtual ve overrider kullanılır
+             * virtual asıl yani base deki overrider kalıtım daki fonksiyonu belirtir
+             */
+            kedi siyam=new kedi();
+            siyam.ses();
+            kopek kurt=new kopek();
+            kurt.ses();
+
+        }
+    }
+    class hayvan
+    {
+        public virtual void ses()
+        {
+            Console.WriteLine("---------");
+        }
+    }
+    class kedi:hayvan
+    {
+        public override void ses()
+        {
+            Console.WriteLine("Miyav");
+        }
+    }
+    class kopek:hayvan
+    {
+        public override void ses()
+        {
+            Console.WriteLine("Hav Hav");
+        }
+    }
+}
